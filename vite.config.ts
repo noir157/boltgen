c8e986831b91434a3161755b4174725e7d9ace78
Noir <89527725+noir157@users.noreply.github.com>
2025-03-12 22:43:54 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {}
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true
   }
 })
